a054359d171cc8348125a09cb31b0cd7a81cf8a0
Jeddy939 <215430041+Jeddy939@users.noreply.github.com>
2026-09-10 08:50:44 +1000

    Restore game menu at root entry

--- index.tsx
+++ index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TeacherPortal from './TeacherPortal';
-import './teacher.css';
+import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <TeacherPortal />
+    <App />
   </React.StrictMode>
 );
